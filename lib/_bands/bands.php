898d4873d190b972a55e6713e053c77cba04f945
Slim <slim@Robyns-iMac.local>
2019-05-18 10:39:39 -0400

    shuffling things around for reorganization

---
+++ lib/_bands/bands.php
@@ -0,0 +1,67 @@
+<?php
+
+require_once APP_ROOT."system/collections/collections.php";
+require_once APP_ROOT."system/_users/users.php";
+require_once APP_ROOT."system/_songs/songs.php";
+
+class Member extends User{
+        
+}
+
+class Band extends Entity{
+    private $members;
+    public $songs;
+    public $shows;
+    public $site;
+    public $manager;
+    public function __construct($key='band', $home=APP_ROOT){
+        parent::__construct($key,$home);
+        //$this->members = new Users(MEMBERS);
+        $this->songs = new Songs($this->self);
+
+    }
+    public function __destruct(){
+
+    }
+    
+    public function get_songs(){
+        $_ = array();
+        $songs = $this->songs->get_songs();
+        $songs = $this->songs->get_collection();
+        foreach($songs as $songid){
+            $_[] = new Song( $songid,$this->songs->self );
+        }
+        return $_;
+    }
+    
+    public function add_song(){
+        
+    }
+    
+    public function get_members(){
+        return $this->members->get_collection();
+    }
+
+    public function delete(){}
+    
+    public function load(){
+        return;
+    }
+}
+
+class Bands extends Collection{
+   
+    
+    public function __construct($args=null){
+        parent::__construct($args);
+    }
+    
+    public function __destruct(){
+        parent::__destruct();
+    }
+  
+    public function get_bands(){
+        return parent::get_collection();
+    }
+        
+}
---
+++ lib/_recordings/recordings.php
@@ -0,0 +1,72 @@
+<?php
+require_once APP_ROOT."/system/collections/collections.php";
+
+class Recording extends Item{
+        public $title;
+        public $location;
+        public $type;
+        public function __construct($key = null, $dir = null){
+            parent::__construct($key,$dir);
+        }
+        public function __destruct(){}
+        public function render(){
+
+            $method = "render_".$this->type;
+            
+            $this->$method();
+        }
+        
+        public function render_embed(){
+            $code = file_get_contents( $this->self."embed.code" );
+            echo  '<h3>'.$this->title.'</h3>';
+            echo $code;
+        }
+        
+        public function render_file(){
+                
+            $url = str_replace(APP_ROOT, "", $this->self.$this->key);
+            
+            $fh = finfo_open(FILEINFO_MIME);
+            $finfo = finfo_file($fh, $this->self.$this->key);
+ 
+            if( strpos( $finfo , "audio" ) > -1 ){
+                $str = '<h3>'.$this->title.'</h3>';
+                $str .= '<audio controls style="width:500px">
+                    <source src="'.BASEURL.$url.'" type="audio/mp3">
+                </audio>';
+                
+                echo $str;
+            }
+        }
+        
+
+}
+
+class Recordings extends Collection{
+        
+        public function __construct($path){
+            parent::__construct($path);
+        }
+        
+        public function __destruct(){}
+        public function get_recordings(){
+            $_ = array();
+            
+            $keys = parent::get_collection($this->self);
+            
+            foreach($keys as $key){
+               
+                $recording = new Recording($key,$this->self);
+                
+                $_[] = $recording;
+            }
+            return $_;
+        }
+        
+        public function add($recording){
+            $recording->save();
+        }
+        
+
+        
+}
